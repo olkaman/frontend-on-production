4b7d53255aea9d92984fbe9337f25d4c76ac0ebb
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-07-21 17:24:38 +0200

    change in app

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
-        <h1 className="text-xl font-bold text-center">🚀 Rick and Morty - Fan Service hello!</h1>
+        <h1 className="text-xl font-bold text-center">
+          🚀 Rick and Morty - Fan Service hello all!
+        </h1>
       </div>
       <Outlet />
     </div>
