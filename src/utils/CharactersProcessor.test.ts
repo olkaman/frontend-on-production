05d0346322a79b34ec23c8571ef4d926ee0c3b41
Aleksandra Manczyk <aleksandra.manczyk@sienn.pl>
2024-07-21 17:41:13 +0200

    update tests not to pass

--- src/utils/CharactersProcessor.test.ts
+++ src/utils/CharactersProcessor.test.ts
@@ -53,7 +53,7 @@ describe('CharactersProcessor', () => {
 
   test('should return two characters', () => {
     const topCharacters = getTopCharacters(characters, 2);
-    expect(topCharacters.length).toBe(2);
+    expect(topCharacters.length).toBe(1);
     expect(topCharacters[0].name).toBe('Rick Sanchez');
     expect(topCharacters[1].name).toBe('Morty Smith');
   });
